59089db259602e8b61e0dcdcb5be17ce2c59d5fe
Jhone-fori-freelance <jhony.fori@gmail.com>
2025-02-18 04:04:03 -0300

    Actualización después del pull

---
+++ Alura-Curso-2025-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+let titulo = document.querySelector("h1");
+titulo.innerHTML ='juego de el numero secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'indica un numero del 1 al 10';
+
+function intentoDeUsuario() {
+    alert('click desde el boton');
+}
